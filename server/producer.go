b9aa8630d240cb9afe4da554beacf81cfc2e633b
帅逼 <281148314@qq.com>
2022-01-03 03:23:47 +0800

    新增生产者websocket连接方式

--- server/producer.go
+++ server/producer.go
@@ -4,8 +4,11 @@ import (
 	"DPMQ/model"
 	"DPMQ/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"sync"
 )
 
+// ProducerSend 以HTTP请求方式接收消息
 // ProducerSend 接收消息生产者发送过来的消息
 func ProducerSend(c *gin.Context) {
 
@@ -29,10 +32,109 @@ func ProducerSend(c *gin.Context) {
 
 	//把消息写入消息通道
 	MessageChan <- message
+	//将消息记录到消息列表
+	MessageList.Store(message.MessageCode, message)
 
 	//发送成功，返回消息标识码
 	c.JSON(0, gin.H{
 		"code": 0,
 		"msg":  message.MessageCode,
 	})
+}
+
+// Producers 连接的消费者客户端,把每个消费者都放进来。Key为{topic}|{consumerId}，topic与consumerId两者之间用字符”|“分隔。Value为websocket连接
+var Producers = make(map[model.Producer]*websocket.Conn)
+
+// ProducersConn 以WebSocket连接方式接收消息
+// ProducersConn 生产者连接，接收生产者发送过来的消息
+func ProducersConn(c *gin.Context) {
+
+	//消息所属主题
+	topic := c.Param("topic")
+	//消费者id
+	producerId := c.Param("producerId")
+	//消费者ip地址
+	producerIp := c.ClientIP()
+
+	//升级get请求为webSocket协议
+	ws, err := UpGrader.Upgrade(c.Writer, c.Request, nil)
+
+	//登录验证
+	for {
+		//连接成功，等待消费者客户端输入访问密钥
+		err = ws.WriteMessage(1, []byte("Please enter the secret key"))
+		if err != nil {
+			Loger.Println(err)
+			return
+		}
+
+		//读取ws中的数据，获取访问密钥
+		_, sk, err := ws.ReadMessage()
+		if err != nil {
+			Loger.Println(err)
+			return
+		}
+		if string(sk) == secretKey {
+			//访问密钥匹配成功
+			err = ws.WriteMessage(1, []byte("Secret key matching succeeded"))
+			if err != nil {
+				Loger.Println(err)
+				return
+			}
+			break
+		}
+
+		//访问密钥匹配失败
+		err = ws.WriteMessage(1, []byte("Secret key matching error"))
+		if err != nil {
+			Loger.Println(err)
+			return
+		}
+	}
+
+	key := model.Producer{}
+	key.ProducerId = producerId
+	key.Topic = topic
+	key.ProducerIp = producerIp
+	key.JoinTime = utils.GetLocalDateTimestamp()
+
+	lock := sync.RWMutex{}
+
+	//将当前连接的消费者放入map中
+	lock.RLock()
+	Producers[key] = ws
+	lock.RUnlock()
+
+	if err != nil {
+		Loger.Println(err)
+		return
+	}
+	defer func(ws *websocket.Conn) {
+		delete(Producers, key) //删除map中的生产者
+		err = ws.Close()
+		if err != nil {
+			Loger.Println(err)
+		}
+	}(ws)
+
+	for {
+		//读取ws中的数据，获取访问密钥
+		_, data, err := ws.ReadMessage()
+		if err != nil {
+			Loger.Println(err)
+			continue
+		}
+		messageData := string(data)
+		message := model.Message{}
+		message.MessageCode = utils.CreateCode(messageData)
+		message.MessageData = messageData
+		message.Topic = topic
+		message.Status = -1
+		message.CreateTime = utils.GetLocalDateTimestamp()
+
+		//把消息写入消息通道
+		MessageChan <- message
+		//将消息记录到消息列表
+		MessageList.Store(message.MessageCode, message)
+	}
 }
