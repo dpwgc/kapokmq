c794cf9f1e2acd157c2535744ceaa9cdc89fd991
帅逼 <281148314@qq.com>
2022-01-04 14:33:47 +0800

    添加延时消息

--- model/model.go
+++ model/model.go
@@ -2,12 +2,13 @@ package model
 
 // Message 消息模板
 type Message struct {
-	MessageCode  string      //消息标识码
-	MessageData  interface{} //消息内容（一般为JSON格式的字符串）
-	Topic        string      //消息所属主题
-	CreateTime   int64       //消息创建时间
-	ConsumedTime int64       //消息被消费时间
-	Status       int         //消息状态（-1：未进入队列,无状态。0：未被消费。1：已被消费）
+	MessageCode  string //消息标识码
+	MessageData  string //消息内容（一般为JSON格式的字符串）
+	Topic        string //消息所属主题
+	CreateTime   int64  //消息创建时间
+	ConsumedTime int64  //消息被消费时间
+	DelayTime    int    //延迟推送时间（单位：秒）
+	Status       int    //消息状态（-1：刚进入队列,无状态。0：消费失败。1：已被消费）
 }
 
 // Consumer 消费者客户端模板
