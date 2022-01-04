eab8d8ece2726c20e9c4073251e7f169565fcaa0
帅逼 <281148314@qq.com>
2022-01-04 15:25:28 +0800

    完善消息模板

--- model/model.go
+++ model/model.go
@@ -1,5 +1,11 @@
 package model
 
+// SendMessage 生产者发送的消息模板
+type SendMessage struct {
+	MessageData string //消息内容（一般为JSON格式的字符串）
+	DelayTime   int64  //延迟推送时间（单位：秒）
+}
+
 // Message 消息模板
 type Message struct {
 	MessageCode  string //消息标识码
@@ -7,7 +13,7 @@ type Message struct {
 	Topic        string //消息所属主题
 	CreateTime   int64  //消息创建时间
 	ConsumedTime int64  //消息被消费时间
-	DelayTime    int    //延迟推送时间（单位：秒）
+	DelayTime    int64  //延迟推送时间（单位：秒）
 	Status       int    //消息状态（-1：刚进入队列,无状态。0：消费失败。1：已被消费）
 }
 
