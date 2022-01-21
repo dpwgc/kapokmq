8f5d1483ea87fc9c87b6964fe939810572ca12f8
帅逼 <281148314@qq.com>
2022-01-22 01:15:47 +0800

    优化消息标识码生成工具

--- utils/createCode.go
+++ utils/createCode.go
@@ -3,15 +3,12 @@ package utils
 import (
 	"fmt"
 	"github.com/go-basic/uuid"
-	"time"
 )
 
-//生成消息标识码
-
+// CreateCode 生成消息标识码
 func CreateCode(messageData string) string {
-	ts := fmt.Sprintf("%x", time.Now().Unix())
 	uu := uuid.New()
-	data := fmt.Sprintf("%s%s%s", messageData, ts, uu)
+	data := fmt.Sprintf("%s%s", messageData, uu)
 	tokenPrefix := Md5Sign(data)
-	return tokenPrefix + ts[:8]
+	return tokenPrefix
 }
