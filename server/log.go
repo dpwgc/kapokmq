b7a7d97a5d50c14c33dc37f574c42725cd1b010d
帅逼 <281148314@qq.com>
2022-01-29 15:28:27 +0800

    日志记录-添加WAL持久化方案

--- server/log.go
+++ server/log.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/viper"
+	"kapokmq/model"
 	"log"
 	"os"
 	"time"
@@ -24,4 +28,34 @@ func InitLog() {
 		}
 	}
 	Loger = log.New(logFile, "", log.LstdFlags|log.Lshortfile|log.LUTC) // 将文件设置为loger作为输出
+}
+
+var isPersistent int
+var WAL *log.Logger
+
+// InitWAL WAL持久化日志
+func InitWAL() {
+
+	//是否开启WAL持久化
+	isPersistent = viper.GetInt("mq.isPersistent")
+	if isPersistent != 2 {
+		return
+	}
+
+	file := "./WAL.log"
+	logFile, _ := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	WAL = log.New(logFile, "", log.LstdFlags|log.Lshortfile|log.LUTC) // 将文件设置为loger作为输出
+}
+
+// SetWAL 消息追加写入日志
+func SetWAL(message model.Message) {
+
+	//如果没有开启WAL持久化
+	if isPersistent != 2 {
+		return
+	}
+
+	//追加写入
+	jsonStr, _ := json.Marshal(message)
+	WAL.Println(fmt.Sprintf("%s%s", "\t\\|SET|\\\t", string(jsonStr)))
 }
