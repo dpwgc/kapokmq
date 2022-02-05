44a9752386512ebbd371197e3f16c83039f8b8cb
帅逼 <281148314@qq.com>
2022-02-05 22:29:44 +0800

    配置模块更新-移除viper依赖

--- config/config.go
+++ config/config.go
@@ -1,15 +1,51 @@
 package config
 
 import (
-	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
+type Conf struct {
+	Server  Server  `yaml:"server"`
+	Mq      Mq      `yaml:"mq"`
+	Cluster Cluster `yaml:"cluster"`
+}
+type Server struct {
+	Addr string `yaml:"addr"`
+	Port string `yaml:"port"`
+}
+type Mq struct {
+	SecretKey         string `yaml:"secretKey"`
+	PushType          int    `yaml:"pushType"`
+	MessageChanBuffer int    `yaml:"messageChanBuffer"`
+	PushMessagesSpeed int    `yaml:"pushMessagesSpeed"`
+	PushCount         int    `yaml:"pushCount"`
+	PushRetryTime     int64  `yaml:"pushRetryTime"`
+	PersistentFile    string `yaml:"persistentFile"`
+	IsPersistent      int    `yaml:"isPersistent"`
+	RecoveryStrategy  int    `yaml:"recoveryStrategy"`
+	PersistentTime    int    `yaml:"persistentTime"`
+	IsCleanConsumed   int    `yaml:"isCleanConsumed"`
+	IsRePush          int    `yaml:"isRePush"`
+	IsClean           int    `yaml:"isClean"`
+	CheckSpeed        int    `yaml:"checkSpeed"`
+	CleanTime         int64  `yaml:"cleanTime"`
+}
+type Cluster struct {
+	IsCluster          int    `yaml:"isCluster"`
+	GossipPort         int    `yaml:"gossipPort"`
+	RegistryAddr       string `yaml:"registryAddr"`
+	RegistryGossipPort string `yaml:"registryGossipPort"`
+}
+
+var Get Conf
+
 func InitConfig() {
-	viper.AddConfigPath("./config")
-	viper.SetConfigFile("application") // 指定配置文件路径
-	viper.SetConfigName("application") //配置文件名
-	viper.SetConfigType("yaml")        //配置文件类型
-	err := viper.ReadInConfig()
+	yamlFile, err := ioutil.ReadFile("application.yaml")
+	if err != nil {
+		panic(err)
+	} // 将读取的yaml文件解析为响应的 struct
+	err = yaml.Unmarshal(yamlFile, &Get)
 	if err != nil {
 		panic(err)
 	}
