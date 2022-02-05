793d6b731291ecd49439dd078f2bd40917734c23
帅逼 <281148314@qq.com>
2022-02-05 22:32:41 +0800

    安全验证

--- middleware/safe.go
+++ middleware/safe.go
@@ -2,7 +2,7 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
+	"kapokmq/config"
 )
 
 // Safe 安全验证中间件
@@ -10,7 +10,7 @@ func Safe(c *gin.Context) {
 
 	secretKey := c.GetHeader("secretKey")
 
-	if secretKey != viper.GetString("mq.secretKey") {
+	if secretKey != config.Get.Mq.SecretKey {
 		c.JSON(-1, gin.H{
 			"code": -1,
 			"msg":  "Secret key matching error",
