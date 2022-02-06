68c72015bf5121d87dcb5292ea2bfd7c3f5e3aed
帅逼 <281148314@qq.com>
2022-02-07 01:39:29 +0800

    生产者连接模块-WAL日志优化

--- server/producer.go
+++ server/producer.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"kapokmq/config"
 	"kapokmq/model"
 	"kapokmq/utils"
 	"strconv"
@@ -19,13 +20,6 @@ var pLock = sync.RWMutex{}
 
 // Producers 连接的生产者客户端，把每个消费者都放进来。Key为Producer结构体，Value为websocket连接
 var Producers = make(map[model.Producer]*websocket.Conn)
-
-// P 消息对象池
-var P = &sync.Pool{
-	New: func() interface{} {
-		return new(model.Message)
-	},
-}
 
 // ProducersConn 以WebSocket连接方式接收消息
 // ProducersConn 生产者连接，接收生产者发送过来的消息
@@ -114,7 +108,7 @@ func ProducersConn(c *gin.Context) {
 		}
 
 		//生成消息模板
-		message := P.Get().(model.Message)
+		message := model.Message{}
 		message.MessageCode = utils.CreateCode(s.MessageData)
 		message.MessageData = s.MessageData
 		message.Topic = topic
@@ -129,8 +123,10 @@ func ProducersConn(c *gin.Context) {
 		message.CreateTime = utils.GetLocalDateTimestamp()
 		message.DelayTime = s.DelayTime
 
-		//持久化：追加写日志方式
-		SetWAL(message)
+		//持久化：WAL写前日志
+		if config.Get.Mq.IsPersistent == 2 {
+			SetWAL(message)
+		}
 
 		//消息写入WAL文件后，向生产者客户端发送确认接收ACK
 		err = ws.WriteMessage(1, []byte("ok"))
@@ -143,9 +139,6 @@ func ProducersConn(c *gin.Context) {
 		MessageList.Store(message.MessageCode, message)
 		//把消息写入消息通道
 		MessageChan <- message
-
-		//将该消息模板放入对象池，等待复用
-		P.Put(message)
 	}
 }
 
