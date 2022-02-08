23363f9fdb8b2b6c06f907a270e963277be77fa3
帅逼 <281148314@qq.com>
2022-02-09 03:40:51 +0800

    路由更新

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"kapokmq/cluster"
+	"kapokmq/console"
 	"kapokmq/middleware"
 	"kapokmq/server"
 	"net/http"
@@ -12,44 +13,47 @@ import (
 func InitRouters() (r *gin.Engine) {
 
 	r = gin.Default()
+
+	//前端页面
 	r.Static("/view", "view")
 	r.LoadHTMLGlob("view/index.html")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	//跨域配置
 	r.Use(middleware.Cors())
 
 	//控制台接口（http post请求，用于查看消息队列的基本信息）
-	console := r.Group("/Console")
-	console.Use(middleware.Safe)
+	consoleApi := r.Group("/Console")
+	consoleApi.Use(middleware.Safe)
 	{
-		console.POST("/Ping", server.Ping)
-		console.POST("/GetClients", server.GetClients)
-		console.POST("/GetConfig", server.GetConfig)
-		console.POST("/GetMessageList", server.GetMessageList)
-		console.POST("/GetMessage", server.GetMessage)
-		console.POST("/DelMessage", server.DelMessage)
-		console.POST("/CountMessage", server.CountMessage)
-		console.POST("/GetNodes", cluster.GetNodes)
+		consoleApi.POST("/Ping", console.Ping)
+		consoleApi.POST("/GetClients", console.GetClients)
+		consoleApi.POST("/GetConfig", console.GetConfig)
+		consoleApi.POST("/GetMessageList", console.GetMessageList)
+		consoleApi.POST("/GetMessage", console.GetMessage)
+		consoleApi.POST("/DelMessage", console.DelMessage)
+		consoleApi.POST("/CountMessage", console.CountMessage)
+		consoleApi.POST("/GetNodes", cluster.GetNodes)
 	}
 
 	//生产者接口（http post请求，用于接收生产者客户端发送的消息）
-	producer := r.Group("/Producer")
-	producer.Use(middleware.Safe)
+	producerApi := r.Group("/Producer")
+	producerApi.Use(middleware.Safe)
 	{
-		producer.POST("/Send", server.ProducerSend)
+		producerApi.POST("/Send", server.ProducerSend)
 	}
 
 	//消费者连接（websocket连接，用于推送消息到消费者客户端）ws://127.0.0.1:8011/Consumers/Conn/test_topic/1
-	consumers := r.Group("/Consumers")
+	consumersConn := r.Group("/Consumers")
 	{
-		consumers.GET("/Conn/:topic/:consumerId", server.ConsumersConn)
+		consumersConn.GET("/Conn/:topic/:consumerId", server.ConsumersConn)
 	}
 	//生产者连接（websocket连接，用于发送消息到消息队列）ws://127.0.0.1:8011/Producers/Conn/test_topic/1
-	producers := r.Group("/Producers")
+	producersConn := r.Group("/Producers")
 	{
-		producers.GET("/Conn/:topic/:producerId", server.ProducersConn)
+		producersConn.GET("/Conn/:topic/:producerId", server.ProducersConn)
 	}
 	return
 }
