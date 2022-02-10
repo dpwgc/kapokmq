94b242be72d5fb00d49643832565824d3a028dff
帅逼 <281148314@qq.com>
2022-02-10 16:57:41 +0800

    主节点同步模块

--- syncConn/master.go
+++ syncConn/master.go
@@ -1,11 +1,11 @@
-package cluster
+package syncConn
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"kapokmq/config"
 	"kapokmq/model"
-	"kapokmq/server"
+	"kapokmq/mqLog"
 	"time"
 )
 
@@ -15,9 +15,9 @@ func Master(c *gin.Context) {
 	defer func(SyncConn *websocket.Conn) {
 		err := SyncConn.Close()
 		if err != nil {
-			server.Loger.Println(err)
+			mqLog.Loger.Println(err)
 		}
-	}(SyncConn)
+	}(Conn)
 
 	//升级get请求为webSocket协议
 	SyncConn, err := UpGrader.Upgrade(c.Writer, c.Request, nil)
@@ -27,21 +27,21 @@ func Master(c *gin.Context) {
 		//连接成功，等待从节点输入访问密钥
 		err = SyncConn.WriteMessage(1, []byte("Please enter the secret key"))
 		if err != nil {
-			server.Loger.Println(err)
+			mqLog.Loger.Println(err)
 			return
 		}
 
 		//读取ws中的数据，获取访问密钥
 		_, sk, err := SyncConn.ReadMessage()
 		if err != nil {
-			server.Loger.Println(err)
+			mqLog.Loger.Println(err)
 			return
 		}
 		if string(sk) == config.Get.Mq.SecretKey {
 			//访问密钥匹配成功
 			err = SyncConn.WriteMessage(1, []byte("Secret key matching succeeded"))
 			if err != nil {
-				server.Loger.Println(err)
+				mqLog.Loger.Println(err)
 				return
 			}
 			break
@@ -50,7 +50,7 @@ func Master(c *gin.Context) {
 		//访问密钥匹配失败
 		err = SyncConn.WriteMessage(1, []byte("Secret key matching error"))
 		if err != nil {
-			server.Loger.Println(err)
+			mqLog.Loger.Println(err)
 			return
 		}
 	}
@@ -63,9 +63,9 @@ func Master(c *gin.Context) {
 
 // SendMessage 主从同步，向从节点发送消息
 func SendMessage(message model.Message) {
-	err := SyncConn.WriteJSON(message)
+	err := Conn.WriteJSON(message)
 	if err != nil {
-		server.Loger.Println(err)
+		mqLog.Loger.Println(err)
 		return
 	}
 }
